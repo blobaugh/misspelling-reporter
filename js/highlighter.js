ba40619cdcbbc3799546c3b1560ddd1585c91f6b
Joachim Kudish <info@jkudish.com>
2013-05-29 23:05:13 -0600

    updates to the js
    minor spacing/whitespace fixes + jQuery optimization
    
    warning: I didn't test this; you should before you merge.

--- js/highlighter.js
+++ js/highlighter.js
@@ -1,10 +1,8 @@
-var selected = 'asdf';
-
 function getSelectionText() {
     var text = "";
-    if (window.getSelection) {
+    if ( window.getSelection ) {
 	text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
+    } else if ( document.selection && document.selection.type != "Control" ) {
 	text = document.selection.createRange().text;
     }
     return text;
@@ -12,41 +10,35 @@ function getSelectionText() {
 
 
 
-jQuery(document).ready(function (){
+jQuery(document).ready(function($){
     
-    jQuery('body').mouseup(function (e){
-	selected = getSelectionText();
-	var word = '';
+    $( 'body' ).on( 'mouseup', function(e){
+		selected = getSelectionText();
+		var word = '';
 	
-	if( '' != selected ) {
-	    var first_word = selected.split(' ');
-	    word = first_word[0];
-	}
+		if ( '' != selected ) {
+	    	var first_word = selected.split(' ');
+	    	word = first_word[0];
+		}
 	    
-	if( '' !=  word) { 
+		if ( '' ==  word )
+			return;
 	    
-	    // Show popdown to report misspelling
-	    jQuery('body').append('<div id="missr_dialog" onclick="missr_clicked(\''+word+'\');" style="cursor: pointer; position: fixed; top: 0px; z-index: 999999999; padding: 7px; background-color: yellow; border: 1px solid darkyellow;"> Click to Report Misspelling</div>');
-	} else {
-	// Hide popdown 
-	}
-    });
-   
-   
+		// Show popdown to report misspelling
+		$( 'body' ).append('<div id="missr_dialog" onclick="missr_clicked(\''+word+'\');" style="cursor: pointer; position: fixed; top: 0px; z-index: 999999999; padding: 7px; background-color: yellow; border: 1px solid darkyellow;"> Click to Report Misspelling</div>');
+	});
    
 });
 
 
-function missr_clicked(text) {
-    
-    
+function missr_clicked( text ) {    
     var data = {
-	action: 'missr_report',
-	post_id: post.post_id,
-	selected: text
+		action: 'missr_report',
+		post_id: post.post_id,
+		selected: text
     };
     
-    jQuery.post(post.ajaxurl, data, function(response) {
-	//console.log('Got this from the server: ' + response);
+    jQuery.post( post.ajaxurl, data, function(response) {
+		//console.log('Got this from the server: ' + response);
 	});
 }
