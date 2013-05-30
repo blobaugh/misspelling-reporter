7b70fbc5073b2181d5d76821c3ba24c178aec073
Joachim Kudish <info@jkudish.com>
2013-05-29 22:55:14 -0600

    fix coding standards
    fix for coding standards (spacing, whitespace, etc.) as well as proper escaping/sanitizations

--- misspelling-reporter.php
+++ misspelling-reporter.php
@@ -8,48 +8,49 @@
  * Author: Ben Lobaugh
  * Author URI: http://ben.lobaugh.net
  */
-define('MISSR_PLUGIN_DIR', trailingslashit(dirname(__FILE__)));
-define('MISSR_PLUGIN_URL', trailingslashit(WP_PLUGIN_URL . "/" . basename(__DIR__)));
-define('MISSR_PLUGIN_FILE', MISSR_PLUGIN_DIR . basename(__DIR__) . ".php");
+define( 'MISSR_PLUGIN_DIR', trailingslashit( dirname( __FILE__ ) ) );
+define( 'MISSR_PLUGIN_URL', trailingslashit( WP_PLUGIN_URL . '/' . basename( __DIR__ ) ) );
+define( 'MISSR_PLUGIN_FILE', MISSR_PLUGIN_DIR . basename( __DIR__ ) . '.php' );
 
 
-add_action('wp_enqueue_scripts', 'missr_enqueue_scripts');
-add_action('wp_ajax_missr_report', 'missr_ajax_report');
-add_action('wp_ajax_nopriv_missr_report', 'missr_ajax_report');
+add_action( 'wp_enqueue_scripts', 'missr_enqueue_scripts' );
+add_action( 'wp_ajax_missr_report', 'missr_ajax_report' );
+add_action( 'wp_ajax_nopriv_missr_report', 'missr_ajax_report' );
 
 function missr_enqueue_scripts() {
     global $post;
 
     // Front end text selection code
     wp_enqueue_script(
-	    'missr_highlighter', MISSR_PLUGIN_URL . '/js/highlighter.js', array('jquery')
+	    'missr_highlighter', MISSR_PLUGIN_URL . '/js/highlighter.js', array( 'jquery' )
     );
 
-    $info = array('post_id' => $post->ID, 'ajaxurl' => admin_url('admin-ajax.php'));
-    wp_localize_script('missr_highlighter', 'post', $info);
+    $info = array( 'post_id' => $post->ID, 'ajaxurl' => admin_url( 'admin-ajax.php' ) );
+    wp_localize_script( 'missr_highlighter', 'post', $info );
 }
 
 function missr_ajax_report() {
 
     $args = array(
 	'post_type' => 'missr_report',
-	'post_title' => esc_attr($_POST['selected'])
+	'post_title' => esc_attr( $_POST['selected'] )
     );
     
+    $reported_post_id = absint( $_POST['post_id'] );
     $post_id = wp_insert_post( $args );
     
-    update_post_meta( $post_id, 'missr_post_id', esc_attr($_POST['post_id']));
+    update_post_meta( $post_id, 'missr_post_id', $reported_post_id );
     
     
-    $post = get_post( $_POST['post_id']);
+    $post = get_post( $reported_post_id );
     
     $subject = "Misspelling Report";
     
-    $body = "Post: " . get_permalink($post->ID );
-    $body .= "\n\nMisspelling: " . $_POST['selected'];
+    $body = "Post: " . get_permalink( $post->ID );
+    $body .= "\n\nMisspelling: " . esc_html( $_POST['selected'] );
 
     // Email site admin
-    wp_mail( get_option('admin_email'), $subject, $body );
+    wp_mail( get_option( 'admin_email' ), $subject, $body );
     
     
     // mail post author
